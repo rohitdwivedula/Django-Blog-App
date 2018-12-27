386ca47770afd30bf4bee7585aacf9e852cf116b
rohitdwivedula <rohitdwivedula@gmail.com>
2018-12-27 20:51:25 +0530

    Added code to dynamically display posts on Home Page view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
+from django.utils import timezone
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = BlogPost.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
