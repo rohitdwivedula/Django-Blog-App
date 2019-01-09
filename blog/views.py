391d43366cc11b38a306c7d2594f34037c84781d
rohitdwivedula <rohitdwivedula@gmail.com>
2019-01-09 11:36:02 +0530

    Added footer and began designing a navbar

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,6 @@ def post_list(request):
 	posts = BlogPost.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(BlogPost, pk=pk)
+def post_detail(request, post_id):
+    post = get_object_or_404(BlogPost, pk=post_id)
     return render(request, 'blog/post_detail.html', {'post': post})
